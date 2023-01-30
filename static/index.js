81887d1691558b837ef7553511c8595406471a5a
AnomalyJx <108090330+AnomalyJx@users.noreply.github.com>
2023-01-30 13:05:40 +0000

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,7 +1,6 @@
 const input = document.querySelector('.galaxy-input');
 const searchBTN = document.querySelector('.search-btn');
 
-
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 
@@ -12,7 +11,7 @@ searchBTN.addEventListener('click', async event => {
         scope: __uv$config.prefix
     }).then(() => {
         let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://search.brave.com/search?q=' + url;
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
         var urle = "https://" + document.domain + __uv$config.prefix + __uv$config.encodeUrl(url);
         if (urle) {
